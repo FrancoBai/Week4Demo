0404bc0e8dc199f03422cb1756209ccc44d3f199
829942 <829942@SAIT225709IT.ACDM.DS.SAIT.CA>
2020-10-04 19:20:46 -0600

    Converted app into a full MVC

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Person;
 
 /**
  *
@@ -26,10 +27,12 @@ public class HelloWorldServlet extends HttpServlet {
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
         
-        request.setAttribute("firstname", firstname);
-        request.setAttribute("lastname", lastname);
+       
         
         if(firstname==null || firstname.equals("")||lastname==null||lastname.equals("")){
+            request.setAttribute("firstname", firstname);
+            request.setAttribute("lastname", lastname);
+            
             request.setAttribute("message", "Invalid entry. Please enter both your first and last name.");
             
             
@@ -38,6 +41,9 @@ public class HelloWorldServlet extends HttpServlet {
             return;
         }
 
+        Person person = new Person(firstname, lastname);
+        request.setAttribute("person",person);
+        
         getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
                 .forward(request, response);
     }
